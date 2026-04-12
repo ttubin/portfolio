812900f47bea5011babae2d2d7887718d62f6078
Soobin Jeong <ttubiny@gmail.com>
2026-04-13 03:23:06 +0900

    update 3:23am

--- src/components/Projects/ProjectCard.tsx
+++ src/components/Projects/ProjectCard.tsx
@@ -19,11 +19,11 @@ export default function ProjectCard({ project }: Props) {
         <p className={styles.date}>{project.period}</p>
         <p className={styles.role}>{project.role}</p>
         <p className={styles.desc}>{project.desc}</p>
-        {project.link && (
+        {/* {project.link && (
           <Link href={project.link} target="_blank" className={styles.link}>
             URL
           </Link>
-        )}
+        )} */}
       </div>
       <div className={styles.card__img}>
         <Image src={project.image} alt={project.title} fill={true} priority />
